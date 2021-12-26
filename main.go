92593e6a9ad599a00275663bf7ee0cc0517dedbc
samuelsih <samuel.205025@mhs.its.ac.id>
2021-12-26 23:16:52 +0700

    success unmarshal json and print it

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
+//Story represents the JSON field
+type Story struct {
+	Title   string            `json:"title"`
+	Story   []string          `json:"story"`
+	Options map[string]string `json:"options"`
+}
 
-// const port = ":8080"
 
+//OpenJsonFile open the json file
+//return *File
 func OpenJsonFile(path string) *os.File {
 	//open json file
 	jsonFile, err := os.Open(path)
@@ -17,15 +26,32 @@ func OpenJsonFile(path string) *os.File {
 		fmt.Println(err)
 	}
 
-	//defer the closing of our jsonFile so that we can parse it later on
-	defer jsonFile.Close()
-
 	return jsonFile
 }
 
 func main() {
+	//get json file
 	jsonFile := OpenJsonFile("tools/story.json")
 
-	fmt.Println("Success reading jsonFile => " + jsonFile.Name())
+	//ioutil.ReadAll() read all the jsonFile data
+	//return it as a []byte
+	byteValue, err := ioutil.ReadAll(jsonFile)
 
+	if err != nil {
+		panic(err)
+	}
+
+	var story map[string]Story
+
+	//Unmarshal parses the JSON-encoded data and stores it in story variable
+	json.Unmarshal(byteValue, &story)
+
+	for key, value := range story {
+		fmt.Println(key)
+		fmt.Println("title = " + value.Title)
+
+		for _, s := range value.Story {
+			fmt.Println(s)
+		}
+	}
 }
